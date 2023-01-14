5148d0054043b1398b00e5b8c0c688d23c1aa125
John Seong <wonmor@gmail.com>
2023-01-14 16:57:56 -0500

    Added code snippets for the individual pages

---
+++ components/Doctor.jsx
@@ -0,0 +1,3 @@
+// Severity rating, 1-5, 5 being the most severe
+// Show the list of patients based upon the priority (which is determined by severity rating and wait time)
+// John
---
+++ components/FrontDesk.jsx
@@ -0,0 +1,3 @@
+// Adding patient to the queue
+// Injury type
+// Sajjad and Kevin
