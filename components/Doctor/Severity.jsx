161e91a468f085cfdf1a3b2933d6d5e87a809d65
John Seong <wonmor@gmail.com>
2023-01-15 09:03:30 -0500

    Update Severity.jsx

--- components/Doctor/Severity.jsx
+++ components/Doctor/Severity.jsx
@@ -1,16 +1,17 @@
 //selecting the emergency type to match with the corresponding doctor
 
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const Severity = (props) => {
   const { navigation } = props;
-
   const { patients } = props.route.params;
 
   const [rating, setRating] = useState(0);
   const [numbers, setNumbers] = useState([]);
+
+  const [howBadSelected, setHowBadSelected] = useState(false);
 
   // This code block only runs once at the beginning...
   useEffect(() => {
@@ -34,12 +35,25 @@ const Severity = (props) => {
               </>
           ))}
         </View>
-        <View style={styles.stars}>
+        <View style={[{marginBottom: 30}, styles.stars]}>
           <Ionicons name="arrow-back-outline" size={32} color="white" />
           <Text style={[{fontFamily: 'Outfit_400Regular'}, styles.label]}>Minor</Text>
           <Text style={[{fontFamily: 'Outfit_400Regular'}, styles.label]}>Urgent</Text>
           <Ionicons name="arrow-forward-outline" size={32} color="white" />
         </View>
+
+        <FlatList
+            contentContainerStyle={styles.listContent}
+            data={patients}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+                <TouchableOpacity onPress={() => {
+                  
+                }} style={styles.listButton} activeOpacity={0.8}>
+                    <Text style={[{fontFamily: 'Outfit_600SemiBold'}, styles.listTextTitle]}>{item}</Text>
+                </TouchableOpacity>
+            )}
+        />
       </View>
     </SafeAreaView>
   );
@@ -76,4 +90,31 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
   },
+  listButton: {
+    marginBottom: 10,
+    backgroundColor: '#334155',
+    padding: 15,
+    borderRadius: 10,
+    borderColor: '#fff',
+    borderWidth: 1,
+    elevation: 1,
+  },
+  listContent: {
+      margin: 10,
+      marginStart: 50,
+      marginEnd: 50
+  },
+  listTextTitle: {
+      textAlign: "center",
+      fontWeight: "bold",
+      fontSize: 24,
+      color: "#fff"
+  },
+  listTextDescription: {
+      textTransform: 'capitalize',
+      textAlign: "center",
+      margin: 5,
+      fontSize: 18,
+      color: "#fff"
+  }
 });
